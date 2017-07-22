97b6cf97e9411ee0a3436af2d7d96579429dfe97
Galen Johnson <baxdar@gmail.com>
2017-07-22 13:33:58 -0600

    commit partial math for los and cover

--- DnD Tools/Assets/_Scripts/LoSCalculator.cs
+++ DnD Tools/Assets/_Scripts/LoSCalculator.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoSCalculator : MonoBehaviour {
+    public GameObject A;
+    public GameObject B;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector3 CalculateClosestCorner(Transform caster, Transform target) {
+        Vector3 temp = new Vector3();
+        Vector3 dif = caster.position - target.position;
+
+        if (dif.x > 0)
+            temp.x = (caster.position.x + (caster.lossyScale.x / 2));
+        else
+            temp.x = (caster.position.x - (caster.lossyScale.x / 2));
+
+        if (dif.y > 0)
+            temp.y = (caster.position.y + (caster.lossyScale.y / 2));
+        else
+            temp.y = (caster.position.y - (caster.lossyScale.y / 2));
+
+        if (dif.z > 0)
+            temp.z = (caster.position.z + (caster.lossyScale.z / 2));
+        else
+            temp.z = (caster.position.z - (caster.lossyScale.z / 2));
+
+        return temp;
+    }
+
+    public bool CalculateLineofSight(Transform caster, Transform target) {
+        Vector3 castOrigin;
+        castOrigin = CalculateClosestCorner(caster, target);
+
+        return false;
+    }
+
+    public float CalculateCover(Transform a, Transform target) {
+        Vector3 castOrigin;
+        castOrigin = CalculateClosestCorner(a, target);
+
+        return 1;
+    }
+
+    void Start() {
+        //CalculateLineofSight(A.transform, B.transform);
+    }
 }

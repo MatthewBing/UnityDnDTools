98eb04c7f7f35723a2adfc0693f2726db8a3fef2
Spacebuilder2020 <alexander-davis@outlook.com>
2017-07-22 12:28:45 -0600

    Made some modifications

---
+++ DnD Tools/Assets/_Scripts/GridMap.cs
@@ -0,0 +1,59 @@
+﻿using Assets.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Assets.Classes
+{
+    class GridMap : IMap
+    {
+        Dictionary<int, Dictionary<int, ITile>> tiles = new Dictionary<int, Dictionary<int, ITile>>();
+
+        class TileInternal : ITile
+        {
+            public int X
+            {
+                get;
+                set;
+            }
+
+            public int Y
+            {
+                get;
+                set;
+            }
+            public int Height
+            {
+                get;
+                set;
+            }
+            public TileInternal(int x, int y, int height)
+            {
+                this.X = x;
+                this.Y = y;
+                this.Height = height;
+            }
+        }
+        
+        public GridMap(int sizeX, int sizeY)
+        {
+            for (int i = 0; i < sizeX; i++)
+            {
+                var dict = new Dictionary<int, ITile>();
+                for (int j = 0; i < sizeY; j++)
+                {
+                    dict.Add(j, new TileInternal(i, j, 4));
+                }
+                tiles.Add(i, dict);
+            }
+        }
+
+        public bool canUnitPassTile(int x, int y, ICharacter character)
+        {
+            return tiles[x][y].Height > character.ClimbingFactor;
+        }
+
+    }
+}

ce4d8c3d48da725f636ae1c546e899fcadb857aa
Galen Johnson <baxdar@gmail.com>
2017-07-22 12:43:53 -0600

    updated ignore and pushing changes

--- DnD Tools/Assets/IMap.cs
+++ DnD Tools/Assets/IMap.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace DndTools.Interfaces
 {
-    public interface IMap : IEnumerable
-    {
+    public interface IMap : IEnumerable {
 
         public bool canUnitPassTile(int x, int y, ICharacter character);
     }

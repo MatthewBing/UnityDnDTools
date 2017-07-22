e0dd3bde2b65e5eb5053a368121e37ad7e19ec05
Spacebuilder2020 <alexander-davis@outlook.com>
2017-07-22 11:39:00 -0600

    Add intial stub for Imap. Will not compile until ICharacter is created.

---
+++ DnD Tools/Assets/IMap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DndTools.Interfaces
+{
+    public interface IMap : IEnumerable
+    {
+
+        public bool canUnitPassTile(int x, int y, ICharacter character);
+    }
+}

23627baeaa2b70560614deda2a98dbc37d676170
Charles <charleswcary@gmail.com>
2017-07-22 15:30:59 -0600

    Dice stuff

---
+++ DnD Tools/Assets/LightFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollow : MonoBehaviour {
+
+    GameObject lastRolledDie;
+    GameObject DiceManager;
+
+    private void Start()
+    {
+        DiceManager = GameObject.Find("DiceManager");
+    }
+    private void Update()
+    {
+        if(DiceManager.GetComponent<DiceRoller>().lastDieRoll == null)
+        {
+            return;
+        }
+        else
+            lastRolledDie = DiceManager.GetComponent<DiceRoller>().lastDieRoll;
+
+        transform.LookAt(lastRolledDie.transform);
+    }
+
+}
